ef53100fdee1b6158c9f68177b3267591aeb741c
Abhay Pratap <54666019+Abhay3008@users.noreply.github.com>
2022-01-11 19:40:01 +0530

    Time: 35 ms (5.59%), Space: 42.1 MB (5.14%) - LeetHub

---
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.java
@@ -0,0 +1,74 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int f = 0;
+    public int sumRootToLeaf(TreeNode root) {
+        List<Integer> res = traverse(root, "");
+        
+        int sum = 0;
+        for(int i = 0;i<res.size();i++)
+            sum += res.get(i);
+        return sum;
+    }
+    
+    List<Integer> traverse(TreeNode root, String n){
+       // System.out.print(n+" ");
+        List<Integer> l = new ArrayList<Integer>();
+        if(root==null) return l;
+        
+        if (root.left==null && root.right==null){
+           // if(f!=0) n = n.subList(0, n.size()-1);
+            char ch = (char)(root.val+'0');
+
+          //  System.out.print(ch+" ");
+          //  f = 1;
+            List<Integer> ls = new ArrayList<Integer>();
+            //String s = String.valueOf(n);
+            int decimal = getDecimal(ch+n);
+            //int decimal=Integer.parseInt(s,2);  
+            //System.out.print(ans+" ");
+            ls.add(decimal);
+         //   System.out.print(decimal+" "+ch+n+" ");
+            return ls;
+        
+        }
+        else{
+             char ch = (char)(root.val+'0');
+           
+            l.addAll(traverse(root.left, ch+n));
+            l.addAll(traverse(root.right, ch+n));
+                return l;
+        }
+        
+        
+    }
+    public  int getDecimal(String b){  
+        System.out.print(b+" ");
+    int d = 0;  
+    int n = 0;  
+        int i = 0;
+    while(true){  
+      if(i>=b.length() ){  
+        break;  
+      } else {  
+          int temp = (int)(b.charAt(i++)-'0')%10;  
+          d += temp*Math.pow(2, n);    
+          n++;  
+       }  
+    }  
+    return d;  
+} 
+}
