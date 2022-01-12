1eb003f4cf3872c45d3973be8af50f6f937138ba
Abhay Pratap <54666019+Abhay3008@users.noreply.github.com>
2022-01-12 13:55:51 +0530

    Time: 19 ms (34.35%), Space: 40.9 MB (34.88%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
@@ -2,25 +2,12 @@ class Solution {
     public int maxConsecutiveAnswers(String answerKey, int k) {
         
         int t = 0, f = 0;
-        int x=0, y = 0, key=k;
+        int x=0, key=k;
+        
         if(answerKey.length()==1) return 1;
+        
         char ch[] = answerKey.toCharArray();
-        /*
-        for(int i = 0;i<ch.length;i++){
-            if(ch[i]=='T'){
-                x++;
-                y=0;
-            }
-            else if(ch[i]=='F'){
-                y++;
-                x=0;
-            }
-            if(x>t)
-                t = x;
-            if(y>f)
-                f = y;
-        }*/
-       // System.out.print(t);
+        
          x = 0;
         Queue<Integer> q = new LinkedList<>();
         for(int l = 0, r = 0;r<ch.length;r++){
@@ -42,11 +29,12 @@ class Solution {
             if(x>t)
                 t=x;    
         }
-        x = 0; 
-        q.clear();
-        for(int l = 0, r = 0;r<ch.length;r++){
+    
+    x = 0; 
+    q.clear();
+    for(int l = 0, r = 0;r<ch.length;r++){
             if(ch[r]=='F'){
-                x++;
+             x++;
             }
             else if(ch[r]=='T'){
                 if(key>0){
@@ -54,12 +42,12 @@ class Solution {
                     q.add(r);
                     key--;
                 }
-                else{
+            else{
                     x = r - q.remove();
                     q.add(r);
                 }
             }
-            if(x>f)
+        if(x>f)
                 f=x;    
         }
 
