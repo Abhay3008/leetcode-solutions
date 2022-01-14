feeb25334117defc8ff515c29bd6ea038c72c2b7
Abhay Pratap <54666019+Abhay3008@users.noreply.github.com>
2022-01-14 10:30:06 +0530

    Time: 19 ms (36.22%), Space: 40.9 MB (38.13%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
@@ -1,57 +1,45 @@
 class Solution {
-    public int maxConsecutiveAnswers(String answerKey, int k) {
-        
-        int t = 0, f = 0;
-        int x=0, key=k;
-        
-        if(answerKey.length()==1) return 1;
-        
-        char ch[] = answerKey.toCharArray();
-        
-         x = 0;
-        Queue<Integer> q = new LinkedList<>();
-        for(int l = 0, r = 0;r<ch.length;r++){
-            if(ch[r]=='T'){
-                x++;
-            }
-            else if(ch[r]=='F'){
-                if(k>0){
-                    x++;
-                    q.add(r);
-                    k--;
-                }
-                else{
-                    
-                    x = r-q.remove();
-                    q.add(r);
-                }
-            }
-            if(x>t)
-                t=x;    
-        }
-    
-    x = 0; 
-    q.clear();
-    for(int l = 0, r = 0;r<ch.length;r++){
-            if(ch[r]=='F'){
-             x++;
-            }
-            else if(ch[r]=='T'){
-                if(key>0){
-                    x++;
-                    q.add(r);
-                    key--;
-                }
-            else{
-                    x = r - q.remove();
-                    q.add(r);
-                }
-            }
-        if(x>f)
-                f=x;    
-        }
+public int maxConsecutiveAnswers(String answerKey, int k) {
 
-        return Math.max(t,f);
-        
+        int t = 0;
+        int x=0, key=k, ans = 0;
+
+        if(answerKey.length()==1) return 1;
+
+        char ch[] = answerKey.toCharArray();
+         char i = 'T';
+        x = 0;
+        Queue<Integer> q = new LinkedList<>();
+        for(int j = 1;j<=2;j++) {
+            if(j==1) i = 'T';
+            else i = 'F';
+            x=0;
+            k = key;
+            for (int r = 0; r < ch.length; r++) {
+                if (ch[r] == i) {
+                    x++;
+                } else {
+                    if (k > 0) {
+                        x++;
+                        q.add(r);
+                        k--;
+                    } else {
+
+                        x = r - q.remove();
+                        q.add(r);
+                    }
+                }
+                if (x > t)
+                    t = x;
+            }
+            if(t>ans)
+                ans = t;
+            t = 0;
+            q.clear();
+        }
+              
+
+        return ans;
+
     }
 }
