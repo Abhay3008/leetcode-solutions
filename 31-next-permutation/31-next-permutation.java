7fdc4126a4874b82092346adf7cd9b21454dec7c
Abhay Pratap <54666019+Abhay3008@users.noreply.github.com>
2022-01-17 23:53:38 +0530

    Time: 1 ms (55.36%), Space: 40.9 MB (6.38%) - LeetHub

--- 31-next-permutation/31-next-permutation.java
+++ 31-next-permutation/31-next-permutation.java
@@ -4,17 +4,27 @@ class Solution {
             if(nums[i]>nums[i-1]){
                 int min = i;
                 for(int j = i+1;j<nums.length;j++){
-                    if(nums[j] > nums[i-1] && nums[j]<nums[min])
+                    if(nums[j] > nums[i-1] && nums[j]<=nums[min])
                         min = j;
                 }
                 
                 int temp = nums[min];
                 nums[min] = nums[i-1];
                 nums[i-1] = temp;
-                Arrays.sort(nums, i, nums.length);
+                reverse(nums, i);
                 return ;
             }
         }
         Arrays.sort(nums);
+    }
+      private void reverse(int[] nums, int start) {
+        int i = start, j = nums.length - 1;
+        while (i < j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
+        }
     }
 }
