329b7a0ed2a62fb40e8cd23620afa20cdddb038a
Abhay Pratap <54666019+Abhay3008@users.noreply.github.com>
2022-01-18 23:59:43 +0530

    Time: 2 ms (19.47%), Space: 2.2 MB (13.68%) - LeetHub

--- 75-sort-colors/75-sort-colors.go
+++ 75-sort-colors/75-sort-colors.go
@@ -1,13 +1,21 @@
 func sortColors(nums []int)  {
-    var pos int = 0
-    for i:=0;i<2;i++ {
-        for  j := pos;j<len(nums);j++ {
-            if(nums[j] == i){
-                var temp = nums[pos]
-                nums[pos] = nums[j]
+    var zero int = 0
+    var two int = len(nums)-1
+    //for i:=0;i<2;i++ {
+        
+        for  j := 0;j<=two;j++ {
+            if(nums[j] == 0){
+                var temp = nums[zero]
+                nums[zero] = nums[j]
                 nums[j] = temp
-                pos++;
+                zero++;
+            } else if(nums[j] == 2){
+                var temp = nums[two]
+                nums[two] = nums[j]
+                nums[j] = temp
+                two--;
+                j--;
             }
-        }
-    }
+            //fmt.Println(nums)
+       }
 }
