22900e7f798cf8da2bc71581ab7de8a08a38d40f
Abhay Pratap <54666019+Abhay3008@users.noreply.github.com>
2022-02-07 00:08:28 +0530

    Time: 2 ms (19.22%), Space: 44.5 MB (10.94%) - LeetHub

--- 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.java
+++ 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.java
@@ -1,9 +1,37 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-   int i = 0;
-   for (int n : nums)
-      if (i < 2 || n > nums[i - 2])
-         nums[i++] = n;
-   return i;
-}
+        int n = nums.length;
+        int count = 0;
+        for(int i = 1;i<n;i++){
+            if(nums[i]==nums[i-1])
+                count++;
+            if(nums[i]!=nums[i-1])
+                count = 0;
+            if(count>=2){
+
+                int j = i,size = n;
+                for(;j<size;j++){
+                    if(nums[j]!=nums[j-1])
+                        break;
+                    n--;
+                }
+               // System.out.println(i+" "+n);
+                shift(i,j, nums);
+                //n--;
+                //if(nums[i]!=nums[i-1]){
+                //    System.out.print(i); count = 0;}
+                count = 0;
+            }
+        }
+        return n;
+    }
+    public void shift(int i,int j, int[] nums){
+
+        while(j<nums.length){
+            nums[i] = nums[j];
+            i++;
+            j++;
+        }
+        return ;
+    }
 }
