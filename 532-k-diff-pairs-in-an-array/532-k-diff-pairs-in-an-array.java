92ad2647c337b410e5b4d0fd07a8725ceceb734c
Abhay Pratap <54666019+Abhay3008@users.noreply.github.com>
2022-02-09 11:29:27 +0530

    Time: 10 ms (58.98%), Space: 45.5 MB (34.80%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.java
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.java
@@ -1,30 +1,25 @@
 class Solution {
     public int findPairs(int[] nums, int k) {
-    /*    Arrays.sort(nums);
-        int l = 0,h = nums.length-1,c=0;
-        while(l<h){
-            if(Math.abs(nums[l]-nums[h])==k){
-                c++;
-                while(l<h&&nums[l]==nums[l+1]) l++;
-                while(l<h&&nums[h]==nums[h-1]) h--;
-                l++;
-                h--;
-            }
-            else if(k>Math.abs(nums[l]-nums[h]))h--;
-            else l++;
-        }
-        return c;*/
         Arrays.sort(nums);
-        int c =0;
+        int c = 0;
         for(int i = 0;i<nums.length;i++){
-            for(int j = i+1;j<nums.length;j++){
-                if(Math.abs(nums[i]-nums[j])==k){
-                    c++;
-                    while(j<nums.length-1&&nums[j]==nums[j+1]) j++;
-                    while(i<nums.length-1&&nums[i]==nums[i+1]) i++;
-                }
+         int l = i+1,h = nums.length-1;
+         while(l<=h){
+             int mid = l+(h-l)/2;
+            if(Math.abs(nums[i]-nums[mid])==k){
+                c++;
+                while(i<nums.length-1&&nums[i]==nums[i+1]) i++;
+                break;
+               // while(l<h&&nums[l]==nums[l+1]) l++;
+                
+                //l++;
+                //h--;
             }
+            else if(k>Math.abs(nums[i]-nums[mid]))l = mid+1;
+            else h=mid-1;
+         }
         }
         return c;
+        
     }
 }
