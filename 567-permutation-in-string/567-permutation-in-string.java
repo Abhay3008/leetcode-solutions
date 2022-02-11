f0860c6234271801eeee5b28e44cd25ed790d2d4
Abhay Pratap <54666019+Abhay3008@users.noreply.github.com>
2022-02-11 12:45:43 +0530

    Time: 4 ms (92.54%), Space: 41.7 MB (37.54%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.java
+++ 567-permutation-in-string/567-permutation-in-string.java
@@ -1,23 +1,34 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
+        if(s1.length()>s2.length()) return false;
             int[] c1=new int[26];
         for(int i=0;i<s1.length();i++){
             c1[s1.charAt(i)-'a']++;
         }
-        for(int i=0;i<=s2.length()-s1.length();i++){
-            int[] c2=new int[26];
-            for(int j=0;j<s1.length();j++){
-        
-                c2[s2.charAt(i+j)-'a']++;
+        int[] c2=new int[26];
+        for(int j=0;j<s1.length();j++){
+                c2[s2.charAt(j)-'a']++;
             }
-            int flag=1;
-            
+        int flag=1;
             for(int k=0;k<26;k++){
                 if(c1[k]!=c2[k])
            
                     flag=0;
                 }
             if(flag==1) return true;
+        int x=0;
+        for(int i=s1.length();i<s2.length();i++){
+            c2[s2.charAt(x++)-'a']--;
+            c2[s2.charAt(i)-'a']++;
+             flag=1;
+            //System.out.print("d");
+            for(int k=0;k<26;k++){
+                if(c1[k]!=c2[k])
+           
+                    flag=0;
+                }
+            if(flag==1) return true;
+            
         }
         return false;
     }
