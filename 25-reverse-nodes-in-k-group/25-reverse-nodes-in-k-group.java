b7b153a56266eba17f2e2b137f5934828146139d
Abhay Pratap <54666019+Abhay3008@users.noreply.github.com>
2022-02-17 11:10:14 +0530

    Time: 0 ms (100.00%), Space: 46.1 MB (7.68%) - LeetHub

--- 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.java
@@ -9,11 +9,7 @@
  * }
  */
 class Solution {
-    public ListNode reverseKGroup(ListNode head, int k) {
-        
-        return reverse(head,k);
-    }
-    public ListNode reverse(ListNode n1, int k){
+    public ListNode reverseKGroup(ListNode n1, int k) {
         if(length(n1,k)==false) return n1;
         ListNode n2 = n1.next;
         ListNode head = n1;
@@ -25,10 +21,11 @@ class Solution {
             n2 = n1.next;
             c++;
         }
-        n1.next = reverse(n1.next,k);
+        n1.next = reverseKGroup(n1.next,k);
         
         return head;
     }
+    
     public Boolean length(ListNode node, int k){
         while(k>0 && node!=null){
             node = node.next;
