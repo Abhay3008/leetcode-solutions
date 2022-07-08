26dff46c71f8c37923099da085ea140c07b931c9
Abhay Pratap <54666019+Abhay3008@users.noreply.github.com>
2022-07-08 09:27:28 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.java
@@ -31,33 +31,44 @@ class GFG {
 }// } Driver Code Ends
 
 
+
 class Solution {
-    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj){
-        HashSet<Integer> h= new HashSet<Integer>();
-        Queue <pair> q= new LinkedList <pair> ();
-        for(int i=0; i<V; i++){
-            if(h.contains(i)) continue;
-            q.add(new pair(i, -1));
-            h.add(i);
-            while(!q.isEmpty()){
-                pair pr = q.poll();
-                for(int k : adj.get(pr.c)){
-                    if(h.contains(k) && k!=pr.p) return true;
-                    if(h.contains(k)) continue;
-                    q.add(new pair(k, pr.c));
-                    h.add(k);
+    // Function to detect cycle in an undirected graph.
+    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        int vis[] = new int[V];
+        Queue<pair> q = new LinkedList<>();
+
+        for(int i = 0;i<V;i++){
+            if(vis[i]!=1){
+                vis[i]=1;
+                q.add(new pair(i,-1));
+                while(!q.isEmpty()){
+                     int child = q.peek().first;
+                     int par = q.peek().second;
+                     
+                     q.remove();
+                    
+                    for(int j: adj.get(child)){
+                        if(vis[j]!=1)
+                        {
+                            q.add(new pair(j,child));
+                            vis[j]=1;
+                        }
+                        else if(par!=j) return true;
+                    }
                 }
             }
-            
         }
         return false;
     }
+   
 }
 class pair{
-    int c;      //curent
-    int p;      //parent
-    pair(int c, int p){
-        this.p= p;
-        this.c=c;
+    int first;
+    int second;
+     pair(int first, int second){
+         this.first = first;
+         this.second = second;
     }
 }
