581a25c5305b4e4021cfd357fd03c6059521fb0d
Abhay Pratap <54666019+Abhay3008@users.noreply.github.com>
2022-07-13 23:12:52 +0530

    Time: 2 ms (58.23%), Space: 54 MB (29.52%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.java
+++ 785-is-graph-bipartite/785-is-graph-bipartite.java
@@ -1,14 +1,18 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
+        //bfs
         Queue<Integer> q = new LinkedList<>();
+        //to give set
         int set[] = new int[graph.length];
         for(int i = 0;i<graph.length;i++){
+            //traversal
             if(set[i]==0){
             q.add(i);
             set[i] = 1;
             while(!q.isEmpty()){
                 int n = q.poll();
                 for(int j: graph[n]){
+                    //mark adjacent with diff set.
                     if(set[j]==0){
                         if(set[n]==1)
                             set[j]=2;
