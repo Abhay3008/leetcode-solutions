94732985b7ea3c072a11ae23eac49092cf1c80f7
Abhay Pratap <Abhaypratapsingh3008@gmail.com>
2022-09-20 12:02:27 +0530

    Time: 199 ms (42.31%), Space: 42.5 MB (30.21%) - LeetHub

--- 79-word-search/79-word-search.java
+++ 79-word-search/79-word-search.java
@@ -5,23 +5,23 @@ class Solution {
         for(int i = 0;i<board.length;i++){
             for(int j = 0;j<board[0].length;j++){
                 if(board[i][j]==word.charAt(0))
-                    if(dfs(board,word,i,j,vis))
+                    if(dfs(board,word,i,j,vis,0))
                         return true;
             }
         }
         return false;
     }
-    public boolean dfs(char[][] board, String word,int i, int j,int[][] vis){
-        if(board[i][j]!=word.charAt(0))
+    public boolean dfs(char[][] board, String word,int i, int j,int[][] vis,int x){
+        if(board[i][j]!=word.charAt(x))
             return false;
-        if(word.length()==1)
+        if(word.length()-1==x)
             return true;
         vis[i][j]=1;
         for(int[] d: dir){
             int r = i+d[0];
             int c = j+d[1];
             if(r>=0 && c>=0 && r<board.length && c<board[0].length && vis[r][c]==0)
-            if(dfs(board,word.substring(1,word.length()),r,c,vis)){
+            if(dfs(board,word,r,c,vis,x+1)){
                 vis[i][j]=0;
                 return true;
             }
