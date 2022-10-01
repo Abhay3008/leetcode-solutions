e74fa030cc255934f151bef61d7ef74657b40c17
Abhay Pratap <Abhaypratapsingh3008@gmail.com>
2022-10-02 00:47:16 +0530

    Time: 2 ms (85.59%), Space: 49.1 MB (66.68%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -1,4 +1,8 @@
 class Solution {
+    /*
+    do bfs from cuirrent index aur dekh ki max kahan tak jaa skta h wahan se aur wahan tak jaa aur update krr ki kya yahan se kahin duur jaa skte hain
+    aur agar lastpos pr aa jae toh last pos ko update krr max kahan tak jaa skte hain.
+    */
     public int jump(int[] nums) {
       int lastpos =0, max=0;
         int i = 0,count=0;
