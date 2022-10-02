905084a3cac70668bf6c3411361c183f9709f241
Abhay Pratap <Abhaypratapsingh3008@gmail.com>
2022-10-02 16:07:16 +0530

    Time: 288 ms (14.49%), Space: 150.8 MB (11.11%) - LeetHub

---
+++ 365-water-and-jug-problem/365-water-and-jug-problem.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean canMeasureWater(int x, int y, int z) {
+        
+        Set<Integer> set = new HashSet<>();
+        Queue<Integer> q = new LinkedList<>();
+        q.add(0);
+        set.add(0);
+        while(!q.isEmpty()){
+            int n = q.poll();
+            if(n+x<=(x+y) && set.add(n+x)){
+                q.add(n+x);
+            }
+            if(n+y<=(x+y) && set.add(n+y)){
+                q.add(n+y);
+            }
+            if(n-x>=0 && set.add(n-x)){
+                q.add(n-x);
+            }
+            if(n-y>=0 && set.add(n-y)){
+                q.add(n-y);
+            }
+            if(set.contains(z))
+                return true;
+        }
+        return false;
+    }
+}
